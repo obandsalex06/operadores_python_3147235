dd9d7408671b33d004ff4f78e91f0e13ceeab47f
dilan <dilan.velandiar@gmail.com>
2025-02-28 09:42:23 -0500

    parentesis

--- operadores.py
+++ operadores.py
@@ -2,15 +2,22 @@
 
 '''
 los operadores en phyton siguen elsiguiente orden jerarquico
-1. / , * , %
-2. + , -
-3. =
-Si hay operaciones de la misma jerarquia, se resuelven de izquierda a derecha
+1.                 (  )
+2.                  **
+3.               / , * , %
+4.                 + , -
+5.                   =
+NOTA1: Si hay operaciones de la misma jerarquia, 
+se resuelven de izquierda a derecha
+
+NOTA2: Si hay parentesis dentro del parentesis 
+se resuelve primero el parentesis interno.
 '''
 
 a = 3
 b = 2
 c = 1
-y = a ** 2 - a * b - 1 / 3 + c
+x = 5
+y = ((2 * a + c) / 7) * (a + (4 * a) / c)
 
 print (y)
