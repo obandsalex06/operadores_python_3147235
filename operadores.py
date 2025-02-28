a70b8f75ad36e8f71eeb2d76e36d949488d3d460
dilan <dilan.velandiar@gmail.com>
2025-02-28 09:05:40 -0500

    Operaciones con la misma jerarquia

--- operadores.py
+++ operadores.py
@@ -5,11 +5,12 @@ los operadores en phyton siguen elsiguiente orden jerarquico
 1. / , * , %
 2. + , -
 3. =
+Si hay operaciones de la misma jerarquia, se resuelven de izquierda a derecha
 '''
 
 a = 3
 b = 2
 c = 1
-y = a * b / 3 + c
+y = a ** 2 - a * b - 1 / 3 + c
 
 print (y)
