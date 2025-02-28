08a2f4cc832936848809a449caa935526277e6f8
dilan <dilan.velandiar@gmail.com>
2025-02-28 08:05:43 -0500

    mi primera expresion en python

---
+++ operadores.py
@@ -0,0 +1,15 @@
+# operadores en phyton
+
+'''
+los operadores en phyton siguen elsiguiente orden jerarquico
+1. / , * , %
+2. + , -
+3. =
+'''
+
+a = 3
+b = 2
+c = 1
+y = a * b / 3 + c
+
+print (y)
