f0501e1386394ae718405a2bd3cae53c077f0e93
dilan <dilan.velandiar@gmail.com>
2025-03-07 09:31:25 -0500

    Ejemplo booleano

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -12,6 +12,8 @@ print(op4)
 
 '''
 Jerarquia definitiva de operadores 
+NOTA: si hay operaciones en el mismo nivel 
+de jerarquia, se resuelven de izquierda a derecha
             1.  ()
             2.  **
             3.  *, /, %
@@ -22,3 +24,13 @@ Jerarquia definitiva de operadores
             8.  OR
             9.  =
 '''
+
+
+op1 = False
+op2 = True
+op3 = False
+op4 = True
+
+resultado = not op1 and (op2 or op3 and not op1) and not op4
+
+print (resultado)
