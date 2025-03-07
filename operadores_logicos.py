fc8f39ab6f0182fd21c8731c2241fa1633314c8b
dilan <dilan.velandiar@gmail.com>
2025-03-07 08:58:31 -0500

    operadores booleanos

---
+++ operadores_logicos.py
@@ -0,0 +1,24 @@
+'''
+Operadores Logicos
+
+'''
+
+op1 = False
+op2 = True
+op3 = op1 and op2 
+
+op4 = not op1
+print(op4)
+
+'''
+Jerarquia definitiva de operadores 
+            1.  ()
+            2.  **
+            3.  *, /, %
+            4.  +, -
+            5.  <, >, <=, >=, !=
+            6.  NOT
+            7.  AND
+            8.  OR
+            9.  =
+'''
